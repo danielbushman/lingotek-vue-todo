4fa0fac1c1f3514c23f63888e60cb7aa5c0432df
Daniel Bushman <me@danielbushman.com>
2018-05-01 17:43:06 -0600

    add 100% test coverage to the store

---
+++ src/store/mutations.spec.js
@@ -0,0 +1,57 @@
+import { cloneDeep } from 'lodash';
+import { createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+
+import getters from './getters';
+import mutations from './mutations';
+import { ADD_TASK, REMOVE_TASK, TOGGLE_TASK } from './mutation-types';
+
+const testTask = { id: 'a', name: 'a', completed: false };
+
+const testStore = {
+  state: {
+    tasks: [testTask],
+  },
+  mutations,
+  getters,
+};
+
+describe('mutations', () => {
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
+  let store;
+  beforeEach(() => {
+    store = new Vuex.Store(cloneDeep(testStore));
+  });
+
+  test('ADD_TASK adds a task, given a name', () => {
+    store.commit(ADD_TASK, 'some task');
+    expect(store.state.tasks[1]).toMatchObject({ name: 'some task' });
+    expect(store.state.tasks[1]).toHaveProperty('id');
+    expect(store.state.tasks[1].completed).toBeFalsy();
+  });
+
+  test('REMOVE_TASK removes the provided task', () => {
+    expect(store.state.tasks).toContainEqual(testTask);
+    store.commit(REMOVE_TASK, testTask);
+    expect(store.state.tasks).not.toContainEqual(testTask);
+  });
+
+  test('TOGGLE_TASK toggles the completed property of a task between true and false', () => {
+    let task;
+    task = store.state.tasks[0];
+    expect(task.completed).toEqual(false);
+    task = store.state.tasks[0];
+    store.commit(TOGGLE_TASK, task);
+
+    task = store.state.tasks[0];
+    expect(task.completed).toEqual(true);
+    task = store.state.tasks[0];
+    store.commit(TOGGLE_TASK, task);
+
+    task = store.state.tasks[0];
+    expect(task.completed).toEqual(false);
+    task = store.state.tasks[0];
+    store.commit(TOGGLE_TASK, task);
+  });
+});
