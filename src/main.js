236bc8216e8607b1ae365305a4efcf61ab0d2f87
Daniel Bushman <me@danielbushman.com>
2018-05-01 15:11:49 -0600

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import Buefy from 'buefy';
 import 'buefy/lib/buefy.css';
 
 import App from './App';
+import store from './store';
 
+Vue.use(Vuex);
 Vue.use(Buefy);
 
 Vue.config.productionTip = false;
@@ -12,5 +15,6 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app',
   components: { App },
+  store,
   template: '<App/>',
 });
