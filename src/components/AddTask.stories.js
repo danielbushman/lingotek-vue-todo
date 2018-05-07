3114f751d36758dad4b3e0314e2661613b9ecfb4
Daniel Bushman <me@danielbushman.com>
2018-05-07 15:42:18 -0600

    add action to AddTask story

--- src/components/AddTask.stories.js
+++ src/components/AddTask.stories.js
@@ -1,8 +1,12 @@
 import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
 
 import AddTask from './AddTask';
 
-storiesOf('AddTask', module).add('without props', () => ({
+storiesOf('AddTask', module).add('with actions', () => ({
   components: { AddTask },
-  template: '<add-task/>',
+  template: '<add-task @addTask="addTask"/>',
+  methods: {
+    addTask: action('addTask'),
+  },
 }));
